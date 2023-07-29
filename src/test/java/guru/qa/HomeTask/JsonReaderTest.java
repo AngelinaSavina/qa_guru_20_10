bf36b4faaff911ef81add5fa5e9e426460977a8d
angelina.savina <savinaangelina60@gmail.com>
2023-07-29 16:58:38 +0300

    QA_GURU_10_Homework

---
+++ src/test/java/guru/qa/HomeTask/JsonReaderTest.java
@@ -0,0 +1,31 @@
+package guru.qa.HomeTask;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JsonReaderTest {
+    ClassLoader classLoader = JsonReaderTest.class.getClassLoader();
+
+    @Test
+    @DisplayName("Jackson parse json")
+    void jacksonReaderJson() throws Exception {
+        InputStream inputStream = classLoader.getResourceAsStream("json_example.json");
+        ObjectMapper objectMapper = new ObjectMapper();
+        assert inputStream != null;
+        JsonNode jsonNode = objectMapper.readTree(new InputStreamReader(inputStream));
+
+        assertThat(jsonNode.get("boolean").asBoolean()).isEqualTo(true);
+        assertThat(jsonNode.get("number").asInt()).isEqualTo(123);
+        assertThat(jsonNode.get("string").asText()).isEqualTo("Hello World");
+
+        List<String> list = List.of("gold", "brown");
+
+    }
+}
